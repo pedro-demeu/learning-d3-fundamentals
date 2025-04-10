0e473b9a723a0d3ab2fe281589f01a375fa00843
pdemeu <pdemeu.pessoal@gmail.com>
2025-04-10 18:10:10 -0300

    feat: create project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./reset.css";
+export default function App() {
+  return (
+    <main
+      style={{
+        backgroundColor: "#111",
+        color: "#fdfdfd",
+        fontFamily: "sans-serif",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+          color: "#fdfdfd",
+          paddingTop: "20vh",
+        }}
+      >
+        Learning D3 Fundamentals
+      </h1>
+
+      
+    </main>
+  );
+}
